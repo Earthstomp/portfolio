8a36f392151492d7a21b3e351f4e0be364261492
Earthstomp <keithong100@gmail.com>
2023-05-30 11:33:15 +0800

    refactor:comment files to support basic deployment 2

--- portfolio/app/components/ProjectPictures.tsx
+++ portfolio/app/components/ProjectPictures.tsx
@@ -10,12 +10,12 @@ export type ProjectPicturesProps = {
 const ProjectPictures = ({ projectId }: ProjectPicturesProps) => {
     // const dir = `/${projectId}`;
 
-    const dir = path.resolve('./public', projectId);
-    const images = fs.readdirSync(dir);
+    // const dir = path.resolve('./public', projectId);
+    // const images = fs.readdirSync(dir);
 
     return (
         <div className="">
-            {images.map(image =>
+            {/* {images.map(image =>
                 // <div className="relative rounded-md my-8 h-80 sm:h-full md:h-[120px] lg:h-[300px] overflow-hidden">
                 //     <Image
                 //         key={image}
@@ -25,16 +25,16 @@ const ProjectPictures = ({ projectId }: ProjectPicturesProps) => {
                 //     // style={{ objectFit: 'cover', objectPosition: 'top' }}
                 //     />
                 // </div>
-                    // <div className="relative rounded-md my-8 h-80 sm:h-[100vh] overflow-hidden">
-                    //     <Image
-                    //         key={image}
-                    //         src={`/${projectId}/${image}`}
-                    //         alt="Unable to load image"
-                    //         fill
-                    //         style={{ objectFit: 'cover', objectPosition: 'top' }}
-                    //     />
-                    // </div>
-            )}
+                <div className="relative rounded-md my-8 h-80 sm:h-[100vh] overflow-hidden">
+                    <Image
+                        key={image}
+                        src={`/${projectId}/${image}`}
+                        alt="Unable to load image"
+                        fill
+                        style={{ objectFit: 'cover', objectPosition: 'top' }}
+                    />
+                </div>
+            )} */}
         </div>
     )
 }
