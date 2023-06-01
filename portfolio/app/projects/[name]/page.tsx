e8af92e5f46eeff813f36a4b9cfeed1b26c7cb62
Earthstomp <keithong100@gmail.com>
2023-06-01 09:04:16 +0800

    fix:change project page height and fix footer position

--- portfolio/app/projects/[name]/page.tsx
+++ portfolio/app/projects/[name]/page.tsx
@@ -16,7 +16,7 @@ const ProjectPage = ({ params: { name } }: any) => {
     const github = project?.github;
 
     return (
-        <div className="h-screen text-left">
+        <div className="text-left">
             <div>
                 <div className="sm: w-full md:w-2/3 lg:w-2/3">
                     <h2 className="sm:text-4xl md:text-5xl lg:text-6xl font-bold">{project?.label}</h2>
@@ -31,9 +31,7 @@ const ProjectPage = ({ params: { name } }: any) => {
                 <div className="sm:w-full md:w-5/6 lg:w-5/6">
                     {/* <RepoCard repoName={github} /> */}
                 </div>
-                <div className="">
-                    <ProjectPictures projectId={project?.id} />
-                </div>
+                <ProjectPictures projectId={project?.id} />
             </div>
 
 
--- portfolio/app/projects/page.tsx
+++ portfolio/app/projects/page.tsx
@@ -12,17 +12,6 @@ const ProjectPage = () => {
             <div className="sm:text-4xl md:text-5xl lg:text-6xl font-bold">
                 Projects
             </div>
-            <div className="rounded-md my-4 overflow-hidden">
-                <Image
-                    src={img}
-                    // src={`/dropandgo/home.png`}
-                    alt="Unable to load image"
-                // height={100}
-                // width={300}
-                // fill // to take size of container
-                // style={{ objectFit: 'cover', objectPosition: 'top' }} // to keep ratio
-                />
-            </div>
 
             <div className="pt-8 gap-12 lg:gap-16 grid sm:grid-cols-1 md:grid-cols-2">
                 {projectDetailData.map(project =>
