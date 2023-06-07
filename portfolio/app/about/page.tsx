58ae4eadfe21cb51bd5abf1ac789385ecbc3995e
Earthstomp <keithong100@gmail.com>
2023-06-07 13:13:38 +0800

    style: rearrange about page divs for breakpoints

--- portfolio/app/about/page.tsx
+++ portfolio/app/about/page.tsx
@@ -14,10 +14,10 @@ export default function Home() {
 
     <div>
       <div>
-        <div className="sm:text-4xl md:leading-tight md:text-5xl lg:leading-tight lg:text-6xl font-bold sm:w-full md:w-7/8 xl:w-3/4">
+        <div className="md:text-cyan-500 lg:text-red-500 xl:text-yellow-300 2xl:text-green-500 sm:text-4xl md:leading-tight md:text-5xl lg:leading-tight lg:text-6xl font-bold sm:w-full md:w-7/8 xl:w-3/4">
           A developer, minimalist, tennis player & tech enthusiast.
         </div>
-        <div className="grid 2xl:grid-cols-3 gap-8">
+        <div className="grid xl:grid-cols-3 gap-8">
           <div className="xl:col-span-2 pt-8 sm:text-md md:text-lg lg:text-xl">
             I'm Keith, a Year 3 Undergraduate studying at the National University of Singapore, majoring in Information Systems. I am currently interning at Bites.io on the NUS Overseas College (NOC) Israel exchange programme.
             <br /><br />
@@ -25,39 +25,42 @@ export default function Home() {
             <br /><br />
             In my spare time, I hunt for good food and travel around the world. I also work as a a professional tennis coach and hitting partner.
           </div>
-          <div className="grid grid-cols-2 2xl:grid-cols-1">
-            <div className="mt-8 w-full h-80 relative group">
-              <div className="h-80 -left-6 -top-6 rounded-lg">
-                <div className="h-full relative z-20">
-                  <Image
-                    className="rounded-lg h-full object-cover grayscale-50 group-hover:grayscale-0 duration-1000 "
-                    src="/KeithSmall.jpg"
-                    height={200}
-                    width={420}
-                    alt="profile pic">
-                  </Image>
-                </div>
-                {/* <div className="inline-block absolute top-5 left-5 h-[320px] w-[420px] border-4 border-cyan-500 rounded-lg group-hover:translate-x-3 group-hover:translate-y-3 transition-transform duration-300"></div> */}
+          {/* <div className="mt-8 grid grid-cols-2 gap-4 2xl:grid-cols-1"> */}
+          <div className="w-full h-80 relative group">
+            <div className="h-80 -left-6 -top-6 rounded-md">
+              <div className="h-full relative z-20">
+                <Image
+                  className="rounded-lg object-cover grayscale-50 group-hover:grayscale-0 duration-1000 "
+                  src="/KeithSmall.jpg"
+                  fill
+                  alt="profile pic">
+                </Image>
               </div>
+              <div className="inline-block absolute top-5 left-5 h-full w-full border-4 border-cyan-500 rounded-lg group-hover:translate-x-3 group-hover:translate-y-3 transition-transform duration-300"></div>
+
             </div>
           </div>
           {/* thunderbolt icons non-2xl breakpoint*/}
-          <div className="2xl:hidden">
-            <ul className="grid grid-cols-2 gap-2">
-              {skills.map(tech =>
-                <li className="flex items-center gap-2" key={tech}>
-                  <span className="text-cyan-500">
-                    <AiFillThunderbolt />
-                  </span>
-                  {tech}
-                </li>
-              )}
-            </ul>
-          </div>
+          {/* <div className="2xl:hidden">
+              <div className="md:text-cyan-500 sm:text-2xl md:leading-tight md:text-3xl lg:leading-tight lg:text-6xl font-bold sm:w-full md:w-7/8 xl:w-3/4">
+                Skills </div>
+              <ul className="grid grid-cols-2 gap-2">
+                {skills.map(tech =>
+                  <li className="flex items-center gap-2" key={tech}>
+                    <span className="text-cyan-500">
+                      <AiFillThunderbolt />
+                    </span>
+                    {tech}
+                  </li>
+                )}
+              </ul>
+            </div> */}
+
+          {/* </div> */}
 
         </div>
 
-        <div className="hidden 2xl:inline-flex pt-12 sm:text-sm md:text-md lg:text-lg uppercase text-cyan-500 tracking-tight">
+        <div className="pt-12 sm:text-sm md:text-md lg:text-lg uppercase text-cyan-500 tracking-tight">
           <span className="pr-2">Skills:</span>
 
           <span>{skills.map(tech => <span className="px-2" key={tech}> {tech} </span>)
